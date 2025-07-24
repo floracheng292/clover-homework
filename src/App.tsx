2fa387f32b7ebe235cff1c15bd5c3f6bde5498fd
Flora Cheng <flora.cheng@skyscanner.net>
2025-07-24 18:14:24 +0800

    fix style issues

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { getBackpack, getToolkit } from './utils/fetchData';
 import Header from '@skyscanner-internal/global-components/header';
 import { logError, logOperationalEvent, logWarn } from 'saddlebag-logger';
 import { getHeaderStrings } from './utils/getHeaderStrings';
-import BpkText, { TEXT_STYLES } from '@skyscanner/backpack-web/bpk-component-text';
+import BpkText from '@skyscanner/backpack-web/bpk-component-text';
+import '@skyscanner/backpack-web/bpk-stylesheets';
+import '@skyscanner/backpack-web/bpk-stylesheets/font';
 
 
 type Toolkit = {id: string; name:string};
@@ -23,37 +25,37 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
+    <div className="app">
       <Header isProductionEnv={false} logger={{logOperationalEvent, logError, logWarn}} strings={getHeaderStrings}/>
       <div className="layout-container">
 
-      <div className="toolkit-container">
-        <div className='toolkit-header'>
-          <BpkText tagName='p'>Toolkit</BpkText>
+        <div className="toolkit-container">
+          <div className='toolkit-header'>
+            <BpkText tagName='p'>UX toolkit</BpkText>
+          </div>
+
+          <div className="toolkit-list-container">
+            {toolkitlist.map(tool => (
+              <div className="item" key={tool.id}>
+                <BpkText key={tool.id}>{tool.name}</BpkText> 
+              </div>
+            ))}
+          </div>
         </div>
 
-        <div className="toolkit-list-container">
-          {toolkitlist.map(tool => (
-            <div className="item">
-              <BpkText key={tool.id}>{tool.name}</BpkText> 
-            </div>
-          ))}
-        </div>
-      </div>
+        <div className="backpack-container">
+          <div className='backpack-header'>
+            <BpkText tagName='p'>Backpack</BpkText>
+          </div>
 
-      <div className="backpack-container">
-        <div className='backpack-header'>
-          <BpkText tagName='p'>Backpack</BpkText>
+          <div className="backpack-list-container">
+            {backpacklist.map(item => (
+              <div className="item" key={item.id}>
+                <BpkText key={item.id}>{item.name}</BpkText> 
+              </div>
+            ))}
+          </div>
         </div>
-
-        <div className="backpack-list-container">
-          {backpacklist.map(item => (
-            <div className="item">
-              <BpkText key={item.id}>{item.name}</BpkText> 
-            </div>
-          ))}
-        </div>
-      </div>
       </div>
     </div>
   );
